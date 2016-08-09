072b9f21f8806f36f6ef6752d6c53c2d87935966
agnesfroeschl <agnes.froeschl@gmx.at>
2016-08-09 20:33:26 +0200

    :P

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,22 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {Video} from './video';
+import {PlaylistComponent} from './playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+    mainHeading = Config.MAIN_HEADING;
+    videos:Array<Video>;
+
+    constructor(){
+        this.videos = [
+            new Video(1, "AngularJS 2", "r1LrtmRYEr4", "Tutorial for AngularJS 2"),
+            new Video(1, "Angular 2", "_-CD_5YhJTA", "Learn Angular 2 from Scratch")
+        ]
+    }
+}
